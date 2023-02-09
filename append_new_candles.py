ac686f976fba5e34854f52b85ae2a4b512fb3357
OlegPozovnoy <pozovnoy.oleg@gmail.com>
2023-02-09 15:15:32 +0300

    remove images

--- append_new_candles.py
+++ append_new_candles.py
@@ -1,13 +1,18 @@
 import Examples.Bars_upd
 import time
 import datetime
-import os
+import sys
+import signal
+
 
 if __name__ == '__main__':
     startTime = time.time()
+    signal.alarm(120)
     try:
         #Examples.Bars_upd.update_all_quotes()
         Examples.Bars_upd.update_all_quotes(to_remove=False, candles_num=10)
+    except:
+        print("error", datetime.datetime.now())
+        sys.exit(1)
     finally:
         print(datetime.datetime.now())
-
