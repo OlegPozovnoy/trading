04c16ff005fb16a43485d04731ff84774c8d8cf6
OlegPozovnoy <pozovnoy.oleg@gmail.com>
2023-12-11 03:54:02 +0300

    added tgorders, updated manualorders

--- manual_order.py
+++ manual_order.py
@@ -20,29 +20,32 @@ state = 1
 max_amount = 1
 pause = 10
 
-
-if barrier_up is None and barrier_down is None:
-    order_code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=3))
-    query = f"""insert into public.orders_my(state, quantity, comment, remains, barrier, max_amount, pause, code)
-    values ({state}, {int(quantity)}, '{code + '_' + order_code}', 0,null,{max_amount},{pause}, '{code}')
-    """
-    print(query)
-    engine.execute(query)
-elif barrier_down is None:
-    order_code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=3))
-    query = f"""insert into public.orders_my(state, quantity, comment, remains, barrier, max_amount, pause, code)
-    values ({state}, {int(quantity)}, '{code + '_' + order_code}', 0,{barrier_up},{max_amount},{pause}, '{code}')
-    """
-    print(query)
-    engine.execute(query)
-else:
-    barriers = np.linspace(barrier_up, barrier_down, order_nums)
-    logger.info(barriers)
-    for barrier in barriers:
+def execute_manual_order(quantity, code, barrier_up, barrier_down, order_nums, state, max_amount, pause):
+    if barrier_up is None and barrier_down is None:
         order_code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=3))
         query = f"""insert into public.orders_my(state, quantity, comment, remains, barrier, max_amount, pause, code)
-        values ({state}, {int(quantity/order_nums)}, '{code+'_'+str(int(barrier))+'_'+order_code}', 0,{barrier},{max_amount},{pause}, '{code}')
+        values ({state}, {int(quantity)}, '{code + '_' + order_code}', 0,null,{max_amount},{pause}, '{code}')
         """
         print(query)
         engine.execute(query)
+    elif barrier_down is None:
+        order_code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=3))
+        query = f"""insert into public.orders_my(state, quantity, comment, remains, barrier, max_amount, pause, code)
+        values ({state}, {int(quantity)}, '{code + '_' + order_code}', 0,{barrier_up},{max_amount},{pause}, '{code}')
+        """
+        print(query)
+        engine.execute(query)
+    else:
+        barriers = np.linspace(barrier_up, barrier_down, order_nums)
+        logger.info(barriers)
+        for barrier in barriers:
+            order_code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=3))
+            query = f"""insert into public.orders_my(state, quantity, comment, remains, barrier, max_amount, pause, code)
+            values ({state}, {int(quantity/order_nums)}, '{code+'_'+str(int(barrier))+'_'+order_code}', 0,{barrier},{max_amount},{pause}, '{code}')
+            """
+            print(query)
+            engine.execute(query)
 
+
+if __name__ == '__main__':
+    execute_manual_order(quantity, code, barrier_up, barrier_down, order_nums, state, max_amount, pause)
