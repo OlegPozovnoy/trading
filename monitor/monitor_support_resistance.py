f40c127bb61f75fd1d146d0eadef6d81a4d93a91
OlegPozovnoy <pozovnoy.oleg@gmail.com>
2024-04-15 16:05:14 +0300

    fix monitor gains query

--- monitor/monitor_support_resistance.py
+++ monitor/monitor_support_resistance.py
@@ -66,8 +66,9 @@ def update_df_monitor():
     # 2) ушли вниз
     # 3) ушли вверх
     df_monitor['to_update'] = (df_monitor['new_state'].isnull()) | \
-                              (df_monitor['new_price'] + df_monitor['std'] < df_monitor['old_start']) | \
-                              (df_monitor['new_price'] - df_monitor['std'] > df_monitor['old_end'])
+                                (df_monitor['old_start'].notnull() &
+                                    ((df_monitor['new_price'] + df_monitor['std'] < df_monitor['old_start']) |
+                                     (df_monitor['new_price'] - df_monitor['std'] > df_monitor['old_end'])))
 
     colpairs = [('new_state', 'state'), ('new_start', 'start'), ('new_end', 'end')]
 
