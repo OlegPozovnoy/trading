1547569478f4b66b0e7817caa4d2447c9a290c9e
OlegPozovnoy <pozovnoy.oleg@gmail.com>
2024-04-15 16:50:35 +0300

    fix monitor gains query

--- monitor/monitor_support_resistance.py
+++ monitor/monitor_support_resistance.py
@@ -86,7 +86,7 @@ def update_df_monitor():
 
     logger.info(df_monitor[columns].head())
 
-    sql.get_table.df_to_sql(df_monitor[columns], "public.df_monitor")
+    sql.get_table.df_to_sql(df_monitor[columns], "df_monitor")
     logger.info("saved df to df_monitor table. Selecting after saving")
     logger.info(sql.get_table.query_to_df("select * from public.df_monitor"))
     return df_monitor[df_monitor['to_update']]
